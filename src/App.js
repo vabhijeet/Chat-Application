95af2e9f0da348a3050813a0cac78d8c0b601178
Abhijeet Veer <veerabhijeet10@gmail.com>
2021-07-13 10:16:22 +0530

    chat-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+    return (
+        <ChatEngine 
+        height="100vh"
+        publicKey={'416a115e-a9e3-40cf-8067-e1d220d1bf0b'}
+        userName={'sports_bot'}
+        userSecret={'1234'}
+        // height="100vh"
+        // projectID="416a115e-a9e3-40cf-8067-e1d220d1bf0b"
+        // username="sports_bot"
+        // userSecreat="1234"
+        />
+    )
 }
 
 export default App;
